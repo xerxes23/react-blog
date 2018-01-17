31feb48e3a31b8ffa35ee5362618e856e424c21c
Joseph Mendoza <josephmendoza23@gmail.com>
2018-01-17 11:04:46 -0500

    not found route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -33,7 +33,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path='/new-post' component={NewPost} />
                     <Route path='/posts' component={Posts} />
-                    <Redirect from='/' to='/posts' />
+                    <Route render={()=>(<h1>Not Found</h1>)} />
+                    {/* <Redirect from='/' to='/posts' /> */}
                 </Switch> 
                 
             </div>
